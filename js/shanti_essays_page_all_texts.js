602f61d47c3df9376c73d10e8159126d2ada771e
Rafael Alvarado <rca2t@virginia.edu>
2014-10-08 09:13:13 -0400

    Updated template.php to work with All Texts View.

--- js/shanti_essays_page_all_texts.js
+++ js/shanti_essays_page_all_texts.js
@@ -1,7 +1,156 @@
 // shanti_essays_page_all_texts.js
 
-.book-view-card {
-  float:left;
-  width: 480px;
-  background:white;
-}
+(function($){
+
+var state       = 'fat-list';
+var stateTypes  = ['fat-list','thin-list','grid']; // Note: these are used in the states object and in classes in the template
+var stateCookie = 'view_state';
+
+Drupal.behaviors.shantiTextsAllTexts = {
+
+  attach: function (context, settings) {
+
+    if ($.cookie(stateCookie)) {
+      state = $.cookie(stateCookie);
+    } else {
+      $.cookie(stateCookie,state);
+    }
+  
+    // We define these here since we may want to do some calculations
+    var states = {
+      'fat-list': {
+          '.views-row': {
+            'float': 'left',
+            'width': '100%',
+            'height': '220px', // Get this from a setting ... Inspect the view for image style
+            'background': 'white',
+            'padding-bottom': '20px',
+            'padding-right': '20px',
+            'margin': '0 0 20px'
+          },
+          '.views-field-title h3': {
+            'font-size': '24px',
+            'margin-right': '10px',
+            'margin-bottom': '5px',
+            'margin-top': '20px'
+          },
+          '.views-field-field-general-featured-image img': {
+            'float': 'left',
+            'margin-right': '20px',
+            'height': '220px',
+            'width': '220px'
+          },
+          '.views-field-field-dc-creator-author-1': {
+            'font-size': '14pt',
+            'font-style': 'italic',
+            'color': 'gray'        
+          },
+          '.views-field-field-dc-description': {
+            'font-size': '14pt',
+            'overflow': 'scroll'
+          }
+      },
+      'thin-list': {
+          '.views-row': {
+            'float': 'left',
+            'width': '100%',
+            'height': '110px',
+            'background': 'white',
+            'padding-bottom': '10px',
+            'padding-right': '20px',
+            'margin': '0 0 10px'
+          },
+          '.views-field-title h3': {
+            'font-size': '18px',
+            'margin-right': '10px',
+            'margin-bottom': '5px',
+            'margin-top': '10px'
+          },
+          '.views-field-field-general-featured-image img': {
+            'float': 'left',
+            'margin-right': '10px',
+            'height': '110px',
+            'width': '110px' 
+          },
+          '.views-field-field-dc-creator-author-1': {
+            'font-size': '12pt',
+            'font-style': 'italic',
+            'color': 'gray'        
+          },
+          '.views-field-field-dc-description': {
+            'font-size': '12pt',
+            'overflow': 'scroll'
+          }
+      },
+      'grid': {  
+          '.views-row': {
+            'width': '48%',
+            'height': '220px',
+            'float': 'left',
+            'overflow': 'hidden',
+            'margin': '10px'
+          },
+          '.views-field-title h3': {
+            'font-size': '20px',
+            'margin-bottom': '5px',
+            'margin-top': '10px'  
+          },
+          '.views-field-field-general-featured-image img': {
+             'height': '220px',    
+             'width': '220px'
+          },
+          '.views-field-field-dc-creator-author-1': {
+            'font-size': '12pt',
+            'font-style': 'italic',
+            'color': 'gray'        
+          },
+          '.views-field-field-dc-description': {
+            'font-size': '12pt',
+          }
+      },
+    };
+    
+    //$('#block-system-main').hide();
+    changeState(state); // Executed with this behavior is attached
+    //$('#block-system-main').show();
+    
+    function changeState(mystate,speed) {
+      speed = typeof speed !== 'undefined' ? speed : 'fast';
+      for (var sel in states[mystate]) {
+        $(sel).transition(states[mystate][sel],speed);
+      }    
+      for (var i in stateTypes) {
+        if (stateTypes[i] == mystate) {
+          $('#view-all-texts-switcher li.'+stateTypes[i]).addClass('on');
+        } else {
+          $('#view-all-texts-switcher li.'+stateTypes[i]).removeClass('on');        
+        }
+      }
+      $.cookie(stateCookie,mystate);
+      state = mystate;
+    }
+
+    // Event handlers for view switch buttons
+    
+    $('li .shanticon-list').click(function(){
+      changeState('fat-list');
+    });
+
+    $('li .shanticon-list4').click(function(){
+      changeState('thin-list');
+    });
+
+    $('li .shanticon-grid').click(function(){
+      changeState('grid');  
+    });
+    
+    
+  },
+
+  detach: function (context, settings) {
+    $.removeCookie(stateCookie);
+  },
+
+};
+  
+})(jQuery);
--- template.php
+++ template.php
@@ -1,25 +1,24 @@
 <?php
 
+define('SHANTI_ESSAYS_PATH',drupal_get_path('module','shanti_essays'));
 define('SHANTI_SARVAKA_TEXTS_PATH',drupal_get_path('theme','shanti_sarvaka_texts'));
 
 function shanti_sarvaka_texts_preprocess_views_view(&$vars) {
   if (isset($vars['view']->name) && $vars['view']->name == 'all_texts') {    
+    $header   = $vars['header'];    $vars['header']   = '';
+    $filters  = $vars['exposed'];   $vars['exposed']  = '';
+    $pager    = $vars['pager'];     $vars['pager']    = '';
+    $btn1 = "<span class='icon shanticon-list'></span>";
+    $btn2 = "<span class='icon shanticon-list4'></span>";
+    $btn3 = "<span class='icon shanticon-grid'></span>";
+    $switch = "<ul id='view-all-texts-switcher'><li class='fat-list'>$btn1</li><li class='thin-list'>$btn2</li><li class='grid'>$btn3</li></ul>";
+    $control_box = "<div class='view-all-texts-control-box'><div class='view-all-texts-control-box-row'><span class='a view-all-texts-control-box-cell'>$header</span><span class='b view-all-texts-control-box-cell'>$filters</span><span class='c view-all-texts-control-box-cell'>$switch</span><span class='d view-all-texts-control-box-cell'>$pager</span></div></div>\n";
+    $vars['attachment_before'] = $control_box;
+    $vars['attachment_after'] = $pager;
+    drupal_add_js(SHANTI_ESSAYS_PATH . '/js/jquery.transit.min.js', 'file');
+    drupal_add_js(SHANTI_SARVAKA_TEXTS_PATH . '/js/jquery.cookie.js', 'file');
     drupal_add_js(SHANTI_SARVAKA_TEXTS_PATH . '/js/shanti_essays_page_all_texts.js', $type = 'file', $media = 'all', $preprocess = FALSE);
     drupal_add_css(SHANTI_SARVAKA_TEXTS_PATH . '/css/shanti_essays_page_all_texts.css', $type = 'file', $media = 'all', $preprocess = FALSE);
   }
 }
 
-/*
-function shanti_sarvaka_texts_form_views_exposed_form_alter(&$form, &$form_state) {
-  $view = &$form_state['view'];
-  if ($view->name == 'all_texts') {
-    kpr($form);
-    kpr($form_state);
-    $form_state['pager_plugin']->options['tags']['previous'] = "FOOBY!";
-    if ($view->total_rows == 0) {
-      // Hide 'results per page' when the view has no results.
-      $form['items_per_page']['#access'] = FALSE;
-    }
-  }
-} 
-*/
