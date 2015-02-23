081805a68a272103261f36c99fdf7a13e8e4cc72
root <root@drupal-dev-s.shanti.virginia.edu>
2015-02-23 10:26:17 -0500

    Converted view to render array

--- template.php
+++ template.php
@@ -2,8 +2,6 @@
 
 define('SHANTI_SARVAKA_TEXTS_PATH',drupal_get_path('theme','shanti_sarvaka_texts'));
 
-// function shanti_sarvaka_texts_preprocess_node(&$vars) {}
-
 function shanti_sarvaka_texts_preprocess_views_view(&$vars) {
 
   if (isset($vars['view']->name) && $vars['view']->name == 'all_texts') {
@@ -13,39 +11,65 @@ function shanti_sarvaka_texts_preprocess_views_view(&$vars) {
     $filters  = $vars['exposed'];   $vars['exposed']  = '';
     $pager    = $vars['pager'];     $vars['pager']    = '';
     
-    // Should be a render array
+    $control_box = array(
+      '#type' => 'container',
+      '#attributes' => array('class' => array('view-all-texts-control-box')),
+      'control_box_row' => array(
+        '#type' => 'container',
+        '#attributes' => array('class' => array('view-all-texts-control-row')),
+        'header' => array(
+          '#markup' => $header,
+          '#prefix'  => "<span class='view-all-texts-control-box-cell-header view-all-texts-control-box-cell'>",
+          '#suffix' => "</span>",        
+        ),
+        'filters' => array(
+          '#markup' => $filters,
+          '#prefix'  => "<span class='view-all-texts-control-box-cell-filters view-all-texts-control-box-cell'>",
+          '#suffix' => "</span>",        
+        ),
+        'switch_list' => array(
+            '#prefix'  => "<span class='view-all-texts-control-box-cell-switch view-all-texts-control-box-cell'>",
+            '#suffix' => "</span>",        
+            '#theme' => 'item_list',
+            '#type'  => 'ul',
+            '#attributes' => array('id' => 'view-all-texts-switcher'),
+            '#items' => array(
+              array(
+                'class' => array('fat-list'),
+                'data'  => "<span id='view-all-texts-fat-list' class='icon shanticon-list'></span>",
+              ), 
+              array(
+                'class' => array('thin-list'),
+                'data'  => "<span id='view-all-texts-thin-list' class='icon shanticon-list4'></span>",              
+              ),
+              array(
+                'class' => array('grid'),
+                'data'  => "<span id='view-all-texts-grid' class='icon shanticon-grid'></span>",              
+              ),
+            ),
+          ),
+        'pager' => array(
+          '#markup' => $pager,
+          '#prefix'  => "<span class='view-all-texts-control-box-cell-pager view-all-texts-control-box-cell'>",
+          '#suffix' => "</span>",
+        ),
+      ),
+    );
     
-    // Create the view layout switcher
-    $btn1 = "<span id='view-all-texts-fat-list'   class='icon shanticon-list'></span>";
-    $btn2 = "<span id='view-all-texts-thin-list'  class='icon shanticon-list4'></span>";
-    $btn3 = "<span id='view-all-texts-grid'       class='icon shanticon-grid'></span>";
-    $switch = "<ul id='view-all-texts-switcher'>"
-      ."<li class='fat-list'>$btn1</li>"
-      ."<li class='thin-list'>$btn2</li>"
-      ."<li class='grid'>$btn3</li>"
-      ."</ul>\n";
-    
-    // Put everything in a new element
-    $control_box = "<div class='view-all-texts-control-box'>"
-      ."<div class='view-all-texts-control-box-row'>"
-      ."<span class='view-all-texts-control-box-cell-header view-all-texts-control-box-cell'>$header</span>"
-      ."<span class='view-all-texts-control-box-cell-filters view-all-texts-control-box-cell'>$filters</span>"
-      ."<span class='view-all-texts-control-box-cell-switch view-all-texts-control-box-cell'>$switch</span>"
-      ."<span class='view-all-texts-control-box-cell-pager view-all-texts-control-box-cell'>$pager</span>"
-      ."</div>"
-      ."</div>\n";
-    
+    $control_box['#attached']['js'] = array(
+      SHANTI_TEXTS_PATH . '/js/jquery.transit.min.js',
+      SHANTI_SARVAKA_TEXTS_PATH . '/js/jquery.cookie.js',
+      SHANTI_SARVAKA_TEXTS_PATH . '/js/shanti_texts_page_all_texts.js',
+    );
+
+    $control_box['#attached']['css'] = array(
+      SHANTI_SARVAKA_TEXTS_PATH . '/css/shanti_texts_page_all_texts.css'    
+    );
+
     // Attach the new element to the array
-    $vars['attachment_before'] = $control_box;
+    $vars['attachment_before'] = drupal_render($control_box);
     $vars['attachment_after'] = $pager;
-    
-    // Add JS and CSS files that will take over behavior
-    // Expects that shanti_texts module is loaded -- IS THIS IS THE .info FILE?
-    drupal_add_js(SHANTI_TEXTS_PATH . '/js/jquery.transit.min.js', 'file');
-    drupal_add_js(SHANTI_SARVAKA_TEXTS_PATH . '/js/jquery.cookie.js', 'file');
-    drupal_add_js(SHANTI_SARVAKA_TEXTS_PATH . '/js/shanti_texts_page_all_texts.js', $type = 'file', $media = 'all', $preprocess = FALSE);
-    drupal_add_css(SHANTI_SARVAKA_TEXTS_PATH . '/css/shanti_texts_page_all_texts.css', $type = 'file', $media = 'all', $preprocess = FALSE);
-  
+        
   }
 
 }
