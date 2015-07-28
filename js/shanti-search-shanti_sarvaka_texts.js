c0c7b1141779d18580788ab66d7171aed1c3ad4d
dukarla <marco@Marcos-MacBook-Pro-2.local>
2015-07-27 21:22:47 -0500

    reset button JS-Testing

---
+++ js/shanti-search-shanti_sarvaka_texts.js
@@ -0,0 +1,36 @@
+	
+(function ($) {
+
+	Drupal.behaviors.shantiTextSearchFlyoutCancel = {
+			attach: function (context, settings) {
+				if(context == window.document) {
+													
+				  var mbsrch = $(".view-filters .form-control");  // the main search input
+			    $(mbsrch).data("holder", $(mbsrch).attr("placeholder"));
+			
+			    // --- focusin - focusout
+			    $(mbsrch).focusin(function () {
+			        $(mbsrch).attr("placeholder", "");
+			        $(".views-reset-button button").show("fast");
+			    });
+			    $(mbsrch).focusout(function () {
+			        $(mbsrch).attr("placeholder", $(mbsrch).data("holder"));
+			        $(".views-reset-button button").hide();
+			
+			        var str = "Enter Search...";
+			        var txt = $(mbsrch).val();
+			
+			        if (str.indexOf(txt) > -1) {
+			            $(".views-reset-button button").hide();
+			            return true;
+			        } else {
+			            $(".views-reset-button button").show(100);
+			            return false;
+			        }
+			    });
+				
+				}
+		  }
+		};
+			
+}(jQuery));
