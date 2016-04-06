16a842d5ed867e36d2317810ed52295db8fbab73
dukarla <artesano@vpn-user-1-32.itc.Virginia.EDU>
2016-04-05 20:39:27 -0500

    update carousel indicators

--- templates/shanti-texts-front-page.tpl.php
+++ templates/shanti-texts-front-page.tpl.php
@@ -56,15 +56,20 @@ while ($r = $rs->fetchObject()) {
           </div>        
         <?php endforeach; ?>
         </div>
-        <div class="control-box">
-          <a data-slide="prev" href="#collection-carousel" class="carousel-control left"><span class="icon"></span></a>
-          <a data-slide="next" href="#collection-carousel" class="carousel-control right"><span class="icon"></span></a>
-        </div>
-        <div class="control-box-2">
-          <button class="btn btn-default btn-sm carousel-pause">
-            <span class="glyphicon glyphicon-pause"></span>
-          </button>
-        </div>
+              <div class="control-box">
+                  <a data-slide="prev" href="#collection-carousel" class="carousel-control left"><span class="icon"></span></a>
+                  <a data-slide="next" href="#collection-carousel" class="carousel-control right"><span class="icon"></span></a>
+              </div><!-- /.control-box -->
+
+              <!-- <div class="control-box-2">
+                  <button class="btn btn-default btn-sm carousel-pause"><span class="glyphicon glyphicon-pause"></span></button>
+              </div>--><!-- /.control-box-2 -->
+
+              <ol class="control-box-3 carousel-indicators">
+                  <li data-target="#collection-carousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#collection-carousel" data-slide-to="1"></li>
+                  <li data-target="#collection-carousel" data-slide-to="2"></li>
+              </ol><!-- /.control-box-3 -->
       </div>
     </div>
   </div>
